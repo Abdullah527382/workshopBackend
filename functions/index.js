9f8c02123e4868b04c637c5d9b28cb15c984be86
z5207998 <z5207998@unsw.edu.au>
2020-09-11 10:31:49 +1000

    ready for react integration

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,28 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const express = require("express");
-// Call the express function to
-// var bodyParser = require('body-parser')
 const app = express(); 
-// app.use(bodyParser.json());
 const firebase = require("firebase");
+// Below is used for our sign-in/out route
+const firebaseConfig = {
+   apiKey: "AIzaSyCRtYaiTnH6GKa97-ygal3u4l-n5WVWHcU",
+   authDomain: "recipieces-e3e25.firebaseapp.com",
+   databaseURL: "https://recipieces-e3e25.firebaseio.com",
+   projectId: "recipieces-e3e25",
+   storageBucket: "recipieces-e3e25.appspot.com",
+   messagingSenderId: "584348442895",
+   appId: "1:584348442895:web:dee8ebd43f832491641e7a",
+   measurementId: "G-4M839PNC02"
+ };
 // Initialize our app:
 admin.initializeApp();
-
+// db = database
+const db = admin.firestore();
+firebase.initializeApp(firebaseConfig);
 // Note: We will be using express to make our code more compact and readable
 app.get("/getRecipes", (request, response) => {
    // How to access our database:
-   admin.firestore().collection("recipes").get().then((data) => {
+   db.collection("recipes").get().then((data) => {
       // console.log("DATA: " + JSON.stringify(data));
          let recipes = []; // new array 
          // Use the high order function 'forEach' to iterate through the array
@@ -47,13 +57,47 @@ app.post("/createNewRecipe", (request, response) => {
       type: request.body.type, 
    };
 
-   admin.firestore().collection('recipes').add(newRecipe).then((data) => {
+   db.collection('recipes').add(newRecipe).then((data) => {
          response.json({ message: `recipe with ID ${data.id} added` });
    })
    .catch((err) => {
       response.status(500).json({ status: "failed", error: err.code });
    })
 });
+
+// Get the recipe via ID
+app.get("/recipe/:recipeID", (request, response) => {
+   db.collection('recipes').doc(request.params.recipeID).get().then(data => {
+      if (data.exists){
+         return response.json({status: "Success", movie: data.data()});
+      } else  
+      return response.status(404).json({status: "Failed", error: "Recipe not found"});
+   }).catch(err => {
+      return response.status(500).json({error: err.code});
+   })
+})
+
+app.post("/signup", (request, response) => {
+   firebase.auth()
+   .createUserWithEmailAndPassword(request.body.email, request.body.password)
+   .then((data) => {
+      return data.user.getIdToken();
+   }).then((token) => {
+      return response.json({ token });
+   }).catch(err => response.status(500).json({ error: err.code }));
+
+})
+
+app.post("/login", (request, response) => {
+   firebase.auth()
+   .signInWithEmailAndPassword(request.body.email, request.body.password)
+   .then((data) => {
+      return data.user.getIdToken();
+   }).then((token) => {
+      return response.json({ token });
+   }).catch(err => response.status(500).json({ error: err.code }));
+
+})
 
 // We can also specify region
 exports.api = functions.https.onRequest(app);
